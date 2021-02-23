b8d03100c510232c8d01be0646b83f8a43665b4e
itsgundie <dicb@mail.ru>
2021-02-24 00:04:16 +0300

    registry and pool classes
    partly implemented registry class, pool class and interface

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
 
 const uint32_t MAX_COMPONENTS = 32;
 
@@ -72,9 +74,72 @@ class System
     template <typename T> void RequireComponent();
 };
 
+class IPool
+{
+    public:
+        virtual ~IPool() {};
+};
+
+template <typename T>
+class Pool: public IPool
+{
+    private:
+        std::vector<T> data;
+    public:
+        Pool(int32_t size = 128)
+        {
+            data.resize(size);
+        }
+        ~Pool() = default;
+        bool isEmpty() const
+        {
+            return(data.empty());
+        }
+        int32_t GetSize() const
+        {
+            return(data.size());
+        }
+        void Resize(int32_t newSize)
+        {
+            data.resize(newSize);
+        }
+        void Clear()
+        {
+            data.clear();
+        }
+        void Add(T object)
+        {
+            data.push_back(object);
+        }
+        void Set(int32_t index, T object)
+        {
+            data[index] = object;
+        }
+        T& Get(int32_t index)
+        {
+            return((data[index]));
+        }
+        T& operator [](uint32_t index)
+        {
+            return(data[index]);
+        }
+};
+
 class Registry
 {
-
+    private:
+        int32_t numEntities = 0;
+        // Vector index is Component type, Pool index is entity ID
+        std::vector<IPool*> componentPools;
+        // Turned On Components per Entity, vector index is entity ID
+        std::vector<Signature> entityComponentSignatures;
+        // Don't need sorting, collection of systems
+        std::unordered_map<std::type_index, System*> systems;
+    public:
+        Registry() = default;
+        // TODO: create/destroy entity, add/remove component from entity
+        //      has component, get component
+        //      add/remove system, has/get system
 };
 
 template <typename T>
