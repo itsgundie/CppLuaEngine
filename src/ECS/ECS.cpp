db168f11263b59db97f91744d8c123c121d4a722
itsgundie <dicb@mail.ru>
2021-02-23 20:52:52 +0300

    ecs bare bones
    basic methods for entity, component and system classes

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -5,3 +5,37 @@ int32_t Entity::GetId() const
 {
     return(id);
 }
+
+
+void System::AddEnitityToSystem(Entity enitity)
+{
+    entities.push_back(enitity);
+};
+
+
+
+void System::RemoveEntityFromSystem(Entity entity)
+{
+    // this is some ridiculous syntax
+    entities.erase(std::remove_if(entities.begin(), entities.end(), 
+    [&entity](Entity other)
+    {
+        return(entity == other)
+    }), 
+    entities.end());
+};
+
+
+
+
+std::vector<Entity> System::GetSystemEntities() const
+{
+    return(enitites);
+};
+
+
+
+const Signature& System::GetComponentSignature() const
+{
+    return(componentSignature);
+};
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -9,18 +9,49 @@ const uint32_t MAX_COMPONENTS = 32;
 
 typedef std::bitset<MAX_COMPONENTS> Signature;
 
-class Component
+struct IComponent
 {
+    protected:
+    static int32_t nextId;
+};
+
+template <typename T>
+class Component: public IComponent
+{
+    static int32_t GetId()
+    {
+        static auto id = nextId++;
+        return(id);
+    }
 
 };
 
 class Entity
 {
     private:
-        int id;
+        int32_t id;
     public:
         Entity(int32_t id): id(id) {};
-        int GetId() const;
+        int32_t GetId() const;
+        Entity(const Entity& entity) = default;
+
+        bool operator ==(const Entity& other) const
+        {
+            return id == other.id;
+        };
+        bool operator !=(const Entity& other) const
+        {
+            return id != other.id;
+        };
+        bool operator >(const Entity& other) const
+        {
+            return id > other.id;
+        };
+        bool operator <(const Entity& other) const
+        {
+            return id < other.id;
+        };
+        Entity& operator =(const Entity& other) = default;
 };
 
 class System
@@ -35,12 +66,23 @@ class System
     void AddEnitityToSystem(Entity enitity);
     void RemoveEntityFromSystem(Entity entity);
     std::vector<Entity> GetSystemEntities() const;
-    Signature& GetComponentSignature() const;
+    const Signature& GetComponentSignature() const;
+
+    // What component type is considered by system
+    template <typename T> void RequireComponent();
 };
 
 class Registry
 {
 
 };
+
+template <typename T>
+void System::RequireComponent()
+{
+    const auto componentId = Component<T>::GetId();
+    componentSignature.set(componentId);
+}
+
 
 #endif
